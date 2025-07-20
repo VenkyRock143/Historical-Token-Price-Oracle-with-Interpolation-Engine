d23c206bb6c114939c933417c4325ba557303b81
VenkyRock143 <venky.balusani1@gmail.com>
2025-07-20 23:01:13 +0530

    done

--- backend/tests/interpolate.test.js
+++
@@ -1,8 +0,0 @@
-const { interpolatePrice } = require('../services/utils');
-
-test('interpolates correctly between prices', () => {
-  const before = { timestamp: 1000, price: 10 };
-  const after = { timestamp: 2000, price: 20 };
-  const result = interpolatePrice(1500, before, after);
-  expect(result).toBe(15);
-});
